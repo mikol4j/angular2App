54c667f8fc26c34a99a8609b6efeab561cd8a505
Mikołaj Jankowski <mikolaj.jankowski1@gmail.com>
2017-02-25 23:10:59 +0100

    new features in table product list

--- app/products/product-list.component.ts
+++ app/products/product-list.component.ts
@@ -5,7 +5,11 @@ import {Component} from '@angular/core'
     templateUrl: 'app/products/product-list.component.html'
 })
 export class ProductListComponent{
-    pageTitle: string = "Product List..."
+    pageTitle: string = "Product List...";
+    imageWidth: number =50;
+    imageMargin: number =2;
+    showImage: boolean = false;
+    listFilter: string = 'cart';
     products: any[] =[
     {
         "productId": 1,
@@ -58,4 +62,7 @@ export class ProductListComponent{
         "imageUrl": "http://openclipart.org/image/300px/svg_to_png/120337/xbox-controller_01.png"
     }
 ]
+toggleImage():void{
+    this.showImage = !this.showImage;
+}
 }
