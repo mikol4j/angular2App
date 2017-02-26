b35056c3082a63347202796038ffff23c64e74b3
Mikołaj Jankowski <mikolaj.jankowski1@gmail.com>
2017-02-26 13:24:53 +0100

    added product service to app component and added access for productlist to product service via dependency injection

--- app/products/product-list.component.js
+++ app/products/product-list.component.js
@@ -5,21 +5,26 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 var core_1 = require("@angular/core");
+var product_service_1 = require("./product.service");
 var ProductListComponent = (function () {
-    function ProductListComponent() {
+    function ProductListComponent(_productService) {
+        this._productService = _productService;
         this.pageTitle = "Product List...";
         this.imageWidth = 50;
         this.imageMargin = 2;
         this.showImage = false;
-        this.listFilter = 'cart';
     }
     ProductListComponent.prototype.toggleImage = function () {
         this.showImage = !this.showImage;
     };
     ProductListComponent.prototype.ngOnInit = function () {
         console.log('In OnInit');
+        this.products = this._productService.getProducts();
     };
     ProductListComponent.prototype.onRatingClicked = function (message) {
         this.pageTitle = 'Product List: ' + message;
@@ -32,7 +37,8 @@ ProductListComponent = __decorate([
         moduleId: module.id,
         templateUrl: 'product-list.component.html',
         styleUrls: ['product-list.component.css']
-    })
+    }),
+    __metadata("design:paramtypes", [product_service_1.ProductService])
 ], ProductListComponent);
 exports.ProductListComponent = ProductListComponent;
 //# sourceMappingURL=product-list.component.js.map
--- app/products/product-list.component.ts
+++ app/products/product-list.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core'
 import {IProduct} from './product';
+import {ProductService} from './product.service';
 
 
 @Component({
@@ -13,14 +14,18 @@ export class ProductListComponent implements OnInit{
     imageWidth: number =50;
     imageMargin: number =2;
     showImage: boolean = false;
-    listFilter: string = 'cart';
-    products: IProduct[] ;
-]
+    listFilter: string;
+    products: IProduct[];
+constructor(private _productService: ProductService)
+{
+
+}
 toggleImage():void{
     this.showImage = !this.showImage;
 }
 ngOnInit(): void{
     console.log('In OnInit');
+    this.products = this._productService.getProducts();
 }
 onRatingClicked(message:string) : void{
     this.pageTitle = 'Product List: ' +  message;
